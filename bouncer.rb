f69a53d0f12e6c3c96f2f0734c0a3915518a7b04
William Durden <williamdurden@Williams-MacBook-Air.local>
2016-02-16 09:39:08 -0600

    hmwk stage 1

---
+++ bouncer.rb
@@ -0,0 +1,21 @@
+def check_age
+  puts "What is your age?"
+  age == gets.chomp
+  if age <=18
+    return puts "You are a hoodlum and detriment to society"
+  elsif age >= 18
+    return puts "You may vote and smoke"
+  elsif age >= 21
+    return puts "You may vote, smoke, and imbibe adult beverages"
+  elsif age >= 25
+    return puts "You may vote, smoke, drink, and rent a car"
+end
+
+def try_again
+  puts "Want me to ask again? (Y/N)"
+  choice = gets.chomp
+  if choice == "Y"
+    return true
+  else
+    return false
+  end
