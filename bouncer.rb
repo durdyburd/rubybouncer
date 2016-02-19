486c80ecd8d92d975919e8e28a90a3b5d964a27c
William Durden <williamdurden@Williams-MacBook-Air.local>
2016-02-19 12:29:15 -0600

    huzzah

--- bouncer.rb
+++ bouncer.rb
@@ -1,14 +1,14 @@
 def check_age
   puts "What is your age?"
-  age == gets.chomp
-  if age <=18
-    return puts "You are a hoodlum and detriment to society"
-  elsif age >= 18
-    return puts "You may vote and smoke"
+  age = gets.chomp.to_i
+  if age >=25
+    puts "You may vote, smoke, drink, and rent a car"
   elsif age >= 21
-    return puts "You may vote, smoke, and imbibe adult beverages"
-  elsif age >= 25
-    return puts "You may vote, smoke, drink, and rent a car"
+    puts "You may vote, smoke, and imbibe adult beverages"
+  elsif age >= 18
+    puts "You may vote and smoke"
+  else age <= 18
+    puts "You are a hoodlum and detriment to society"
   end
 end
 
@@ -16,10 +16,11 @@ def try_again
   puts "Want me to ask again? (Y/N)"
   choice = gets.chomp
   if choice == "Y"
-    return true
+    return check_age
   else
     return false
   end
-
-puts check_age
 end
+
+check_age
+try_again
